406e0466ee49056939044701df1ad9e731f414b2
Justin Sostmann <justin.sostmann@googlemail.com>
2020-07-30 17:57:07 +0200

    Updated DropChanceMultiplier, added a Slider

--- DropChanceMultiplier/__init__.py
+++ DropChanceMultiplier/__init__.py
@@ -1,5 +1,6 @@
 import unrealsdk
 from unrealsdk import *
+from ..OptionManager import Options
 
 
 class DropChanceMultiplier(unrealsdk.BL2MOD):
@@ -7,21 +8,29 @@ class DropChanceMultiplier(unrealsdk.BL2MOD):
     Description = "Runs multiple times the function that decides whether or not the Enemy should drop loot."
     Author = "Juso"
 
+    Options = [
+        Options.Slider("Drop Multiplier", "How often should loot be dropped from the same Lootpool.", 1, 0, 100, 1),
+    ]
+
+    def __init__(self):
+        self.multiplier = 0
+
     def Enable(self):
+        def Loot(caller: UObject, function: UFunction, params: FStruct) -> bool:
+            for _ in range(self.multiplier):
+                unrealsdk.DoInjectedCallNext()
+                caller.DropLootOnDeath(params.Killer, params.DamageType, params.DamageTypeDefinition)
+            return True
+
         unrealsdk.RegisterHook("WillowGame.WillowPawn.DropLootOnDeath", "LootHook", Loot)
 
     def Disable(self):
         unrealsdk.RemoveHook("WillowGame.WillowPawn.DropLootOnDeath", "LootHook")
 
-
-LootInstance = DropChanceMultiplier()
-
-
-def Loot(caller: UObject, function: UFunction, params: FStruct) -> bool:
-    for _ in range(5):
-        unrealsdk.DoInjectedCallNext()
-        caller.DropLootOnDeath(params.Killer, params.DamageType, params.DamageTypeDefinition)
-    return True
+    def ModOptionChanged(self, option, newValue):
+        if option in self.Options:
+            if option.Caption == "Drop Multiplier":
+                self.multiplier = newValue
 
 
-unrealsdk.Mods.append(LootInstance)
+unrealsdk.RegisterMod(DropChanceMultiplier())
