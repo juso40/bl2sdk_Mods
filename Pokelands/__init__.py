7b38162a99d6aa510abe755567e5bd10ad410530
Justin Sostmann <justin.sostmann@googlemail.com>
2020-08-23 12:18:23 +0200

    Updated Pokelands

--- Pokelands/__init__.py
+++ Pokelands/__init__.py
@@ -1,8 +1,8 @@
-import bl2sdk
+import unrealsdk
 import webbrowser
 
 
-class Poke(bl2sdk.BL2MOD):
+class Poke(unrealsdk.BL2MOD):
     Name = "Pokelands"
     Description = "Turns the Nasty Suprise into a Pokeball." \
                   " After Killing an enemy your Pokeball will be able to spawn that enemy as and friendly NPC." \
@@ -24,26 +24,26 @@ class Poke(bl2sdk.BL2MOD):
             webbrowser.open("https://www.nexusmods.com/borderlands2/mods/234")
 
     def initial_setup(self):
-        bl2sdk.LoadPackage("Orchid_WormBelly_Dynamic")
-        bl2sdk.LoadPackage("DamTop_Dynamic")
-        turret = bl2sdk.FindObject("Object", "GD_RolandNPC.Projectiles.Projectile_RolandTurret_DamTop:"
-                                             "BehaviorProviderDefinition_0.Behavior_SpawnFromPopulationSystem_39."
-                                             "PopulationFactoryBalancedAIPawn_0")
-        rakkhive = bl2sdk.FindObject("AIPawnBalanceDefinition", "GD_Orchid_Pop_RakkHive.Character."
-                                                                "PawnBalance_Orchid_RakkHive")
-        pawn_rakkhive = bl2sdk.FindObject("WillowAIPawn", "GD_Orchid_RakkHive.Character.Pawn_RakkHive")
-        bl2sdk.KeepAlive(turret)
-        bl2sdk.KeepAlive(rakkhive)
-        bl2sdk.KeepAlive(pawn_rakkhive)
+        unrealsdk.LoadPackage("Orchid_WormBelly_Dynamic")
+        unrealsdk.LoadPackage("DamTop_Dynamic")
+        turret = unrealsdk.FindObject("Object", "GD_RolandNPC.Projectiles.Projectile_RolandTurret_DamTop:"
+                                                "BehaviorProviderDefinition_0.Behavior_SpawnFromPopulationSystem_39."
+                                                "PopulationFactoryBalancedAIPawn_0")
+        rakkhive = unrealsdk.FindObject("AIPawnBalanceDefinition", "GD_Orchid_Pop_RakkHive.Character."
+                                                                   "PawnBalance_Orchid_RakkHive")
+        pawn_rakkhive = unrealsdk.FindObject("WillowAIPawn", "GD_Orchid_RakkHive.Character.Pawn_RakkHive")
+        unrealsdk.KeepAlive(turret)
+        unrealsdk.KeepAlive(rakkhive)
+        unrealsdk.KeepAlive(pawn_rakkhive)
 
     def get_pc(self):
-        return bl2sdk.GetEngine().GamePlayers[0].Actor
+        return unrealsdk.GetEngine().GamePlayers[0].Actor
 
     def HandleKill(self, caller, function, params):
         if params.Killer == self.get_pc():
-            spawn = bl2sdk.FindObject("Object", "GD_Orchid_RakkHive.Animation.Anim_RakkHive_Shake:"
-                                               "BehaviorProviderDefinition_0.Behavior_AISpawn_43")
-            for popdef in bl2sdk.FindAll("WillowPopulationDefinition"):
+            spawn = unrealsdk.FindObject("Object", "GD_Orchid_RakkHive.Animation.Anim_RakkHive_Shake:"
+                                                   "BehaviorProviderDefinition_0.Behavior_AISpawn_43")
+            for popdef in unrealsdk.FindAll("WillowPopulationDefinition"):
                 for i in popdef.ActorArchetypeList:
                     try:
                         WillowAiPawn = i.SpawnFactory.PawnBalanceDefinition.AIPawnArchetype
@@ -52,27 +52,27 @@ class Poke(bl2sdk.BL2MOD):
                             break
                     except:
                         continue
-            desc = bl2sdk.FindObject("Object", "GD_GrenadeMods.Delivery.Delivery_NastySurprise:"
-                                               "AttributePresentationDefinition_4")
-            desc.Description = "Catch 'em all!<br>Currently holding: " + str(caller.CurrentNameTag.NameTag)
+
+            self.get_pc().ConsoleCommand(
+                f"set GD_GrenadeMods.Delivery.Delivery_NastySurprise:AttributePresentationDefinition_4 Description "
+                f"Catch 'em all!<br>Currently holding: {caller.CurrentNameTag.NameTag}",
+                False)
         return True
 
-    KillHook = "WillowGame.WillowAIPawn.Died"
-
     def Enable(self):
+        def KilledHook(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
+            self.HandleKill(caller, function, params)
+            return True
+
         self.initial_setup()
-        bl2sdk.RegisterHook(self.KillHook, "KillHook", KilledHook)
+        unrealsdk.RegisterHook("WillowGame.WillowAIPawn.Died", "KillHook", KilledHook)
+        self.get_pc().ConsoleCommand("set GD_GrenadeMods.Title.Title_NastySurprise PartName Pokeball", False)
+        self.get_pc().ConsoleCommand(
+            f"set GD_GrenadeMods.Delivery.Delivery_NastySurprise:AttributePresentationDefinition_4 Description "
+            f"Catch 'em all!", False)
 
     def Disable(self):
-        bl2sdk.RemoveHook(self.KillHook, "KillHook")
+        unrealsdk.RemoveHook(self.KillHook, "KillHook")
 
 
-PokeInstance = Poke()
-
-
-def KilledHook(caller: bl2sdk.UObject, function: bl2sdk.UFunction, params: bl2sdk.FStruct) -> bool:
-    PokeInstance.HandleKill(caller, function, params)
-    return True
-
-
-bl2sdk.Mods.append(PokeInstance)
+unrealsdk.RegisterMod(Poke())
