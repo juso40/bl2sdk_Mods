c23d71ba8183902f5124caec43e4cd353b421523
Justin Sostmann <justin.sostmann@googlemail.com>
2021-02-06 19:58:33 +0100

    uploaded Photomode.zip

--- dialograndomizer/__init__.py
+++ dialograndomizer/__init__.py
@@ -1,34 +1,51 @@
-import unrealsdk
-
 import random
+from typing import List
+
+import unrealsdk
 
 
 class DialogRando(unrealsdk.BL2MOD):
     Name = "Dialog Randomizer"
-    Version = "1.0"
+    Version = "2.0"
     Author = "Juso"
     Description = "Randomizes Dialogs." \
                   f"\n\nVersion: {Version}"
 
     def __init__(self):
-        pass
+        self.ak_events: List[unrealsdk.UObject] = []
 
     def Enable(self):
-        def talk(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
-            dialog_acts = unrealsdk.FindAll("WillowDialogAct_Talk")[1:]
-            for talk_event in caller.TalkData:
-                try:
-                    talk_event.TalkAkEvent = random.choice(
-                        [x.TalkAkEvent for x in random.choice(dialog_acts).TalkData]
-                    )
-                except:
-                    pass
+        def map_loaded(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
+            pc = unrealsdk.GetEngine().GamePlayers[0].Actor
+            if pc and pc.Pawn:
+                self.ak_events = [ak for ak in unrealsdk.FindAll("AkEvent")[1:] if ak.bVoice]
             return True
 
-        unrealsdk.RegisterHook("WillowGame.WillowDialogAct_Talk.Activate", "Talk", talk)
+        def talk(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
+            for talk_event in caller.TalkData:
+                talk_event.TalkAkEvent = random.choice(self.ak_events)
+
+            return True
+
+        def event(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
+            if caller.Group:
+                for acts in caller.Group.TalkActs:
+                    try:
+                        for data in acts.TalkData:
+                            data.TalkAkEvent = random.choice(self.ak_events)
+                    except:
+                        pass
+            return True
+
+        unrealsdk.RegisterHook("WillowGame.WillowDialogAct_Talk.Activate", f"{__file__}Talk", talk)
+        unrealsdk.RegisterHook("GearboxFramework.Behavior_TriggerDialogEvent.ApplyBehaviorToContext",
+                               f"{__file__}Event", event)
+        unrealsdk.RegisterHook("WillowGame.WillowHUD.CreateWeaponScopeMovie", f"{__file__}TalkMapLoaded", map_loaded)
 
     def Disable(self):
-        unrealsdk.RemoveHook("WillowGame.WillowDialogAct_Talk.Activate", "Talk")
+        unrealsdk.RemoveHook("WillowGame.WillowDialogAct_Talk.Activate", f"{__file__}Talk")
+        unrealsdk.RemoveHook("GearboxFramework.Behavior_TriggerDialogEvent.ApplyBehaviorToContext", f"{__file__}Event")
+        unrealsdk.RemoveHook("WillowGame.WillowHUD.CreateWeaponScopeMovie", f"{__file__}TalkMapLoaded")
 
 
 unrealsdk.RegisterMod(DialogRando())
