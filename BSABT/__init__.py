85aefc306a134625b0924127fb41a1c666bbd7c0
Chronophylos <accounts@chronophylos.com>
2021-03-13 12:57:44 +0100

    use new SKDMod from ModMenu

--- BSABT/__init__.py
+++ BSABT/__init__.py
@@ -4,21 +4,26 @@ import os
 
 from . import travel
 from . import betterspawns
+from Mods.ModMenu import SDKMod, EnabledSaveType, Keybind
 
-
-class Main(BL2MOD):
-    Name = "BSABT"
-    Description = "<B><U><font size='14' color='#e8131d'>Better Spawns and Better Travel</font></U></B>\n" \
+class Main(SDKMod):
+    Name: str = "BSABT"
+    Description: str = "<B><U><font size='14' color='#e8131d'>Better Spawns and Better Travel</font></U></B>\n" \
                   "This Mod reimplements some of the BL3 QoL features, such as spawning at the last respawn station " \
                   "you triggered in game, allowing you to open the FT list form anywhere and directly spawning in " \
                   "your car or near a FT Station directly from the map menu. To teleport in your car or near a FT " \
                   "simply place and remove a waypoint near the car/FT on your map. To open the FT menu press the (by " \
                   "default) F1 key."
-    Author = "Juso"
+    Author: str = "Juso"
+    
+    Types: ModTypes = ModTypes.Utility
+    SaveEnabledState: EnabledSaveType = EnabledSaveType.LoadWithSettings
 
-    Keybinds = [["Show FT", "F1"], ]
+    Keybinds = [Keybind("Show FT", "F1")]
 
     def __init__(self):
+        super().__init__()
+
         self.FILE_PATH = os.path.dirname(os.path.realpath(__file__))
         self.Travel = travel.MapFT()
         self.Spawns = betterspawns.Spawns(self.FILE_PATH)
