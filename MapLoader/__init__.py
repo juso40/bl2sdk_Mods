8f7797e5fa82329a3060e9cef4f9d64fbfa76325
Justin Sostmann <justin.sostmann@googlemail.com>
2021-08-21 12:05:11 +0200

    updated PyImgui

--- MapLoader/__init__.py
+++ MapLoader/__init__.py
@@ -1,6 +1,7 @@
 from typing import List
 import os
 import json
+import threading
 
 import unrealsdk
 from unrealsdk import *
@@ -13,7 +14,7 @@ from . import placeablehelper
 
 class MapLoader(SDKMod):
     Name = "Map Loader"
-    Version = "1.0"
+    Version = "1.1"
     Types = ModTypes.Utility | ModTypes.Content
     Description = "Allows the use of custom map files created by the MapEditor.\n " \
                   "To add/remove a custom map simply place/remove the .json map file into/from the <MapLoader/Maps/> " \
@@ -24,10 +25,17 @@ class MapLoader(SDKMod):
 
     def __init__(self):
         self.path: os.PathLike = os.path.dirname(os.path.realpath(__file__))
-        self.Options: List[OptionManager.Options.Base] = []
+
+        self.loading_option = OptionManager.Options.Spinner(Caption="Threaded Loading",
+                                                            Description="Faster travel times, "
+                                                                        "instead slowly load in custom map changes.",
+                                                            StartingValue="Disabled",
+                                                            Choices=["Enabled", "Disabled"])
+        self.Options: List[OptionManager.Options.Base] = [self.loading_option]
         self.available_maps: List[str] = [os.path.splitext(file)[0] for file
                                           in os.listdir(os.path.join(self.path, "Maps"))
                                           if file.lower().endswith(".json")]
+        self.loader_threads: List[threading.Thread] = []
 
         for amap in self.available_maps:  # type: str
             self.Options.append(
@@ -39,19 +47,49 @@ class MapLoader(SDKMod):
 
     def Enable(self) -> None:
         def end_load(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
+            unrealsdk.RegisterHook("WillowGame.WillowGameViewportClient.Tick", __file__, thread_tick)
+
             level_name: str = bl2tools.get_world_info().GetStreamingPersistentMapName().lower()
             for op in self.Options:  # type: OptionManager.Options.Spinner
                 # Check if map gets modified using its description we earlier set
                 if op.CurrentValue == "Enabled" and level_name in op.Description:
-                    self.load_map(op.Caption, level_name)
+                    if self.loading_option.CurrentValue == "Enabled":
+                        t = threading.Thread(target=self.load_map, args=(op.Caption, level_name))
+                        self.loader_threads.append(t)
+                        t.start()
+                    else:
+                        self.load_map(op.Caption, level_name)
+            return True
+
+        def start_load(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
+            if params.MovieName is None:
+                return True
+            if self.loader_threads:
+                for t in self.loader_threads:
+                    t.join()
+                self.loader_threads.clear()
+                unrealsdk.RemoveHook("WillowGame.WillowGameViewportClient.Tick",
+                                     __file__)
+            return True
+
+        def thread_tick(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
+            if self.loader_threads and all(not x.is_alive() for x in self.loader_threads):
+                self.loader_threads.clear()
+                unrealsdk.RemoveHook("WillowGame.WillowGameViewportClient.Tick",
+                                     __file__)
             return True
 
         unrealsdk.RegisterHook("WillowGame.WillowPlayerController.WillowClientDisableLoadingMovie",
                                __file__,
                                end_load)
+        unrealsdk.RegisterHook("WillowGame.WillowPlayerController.WillowClientShowLoadingMovie",
+                               __file__,
+                               start_load)
 
     def Disable(self) -> None:
         unrealsdk.RemoveHook("WillowGame.WillowPlayerController.WillowClientDisableLoadingMovie",
+                             __file__)
+        unrealsdk.RemoveHook("WillowGame.WillowPlayerController.WillowClientShowLoadingMovie",
                              __file__)
 
     def get_modified_maps(self, name: str) -> List[str]:
