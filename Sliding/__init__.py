eb91162f3a424f1ba02e14ac3a19a38dd8029693
Justin Sostmann <justin.sostmann@googlemail.com>
2021-08-22 17:53:13 +0200

    updated Sliding

--- Sliding/__init__.py
+++ Sliding/__init__.py
@@ -1,13 +1,18 @@
 import unrealsdk
 from unrealsdk import *
 
+from ..ModMenu import SDKMod, Hook, EnabledSaveType, ModTypes
+
 from . import bl2tools
 
 
-class Sliding(unrealsdk.BL2MOD):
+class Sliding(SDKMod):
     Name = "Sliding"
     Description = "Sliding in BL2."
     Author = "Juso"
+    Version = "2.0"
+    Types = ModTypes.Gameplay
+    SaveEnabledState = EnabledSaveType.LoadOnMainMenu
 
     def __init__(self):
         self.slide_duration = 2.0
@@ -15,33 +20,39 @@ class Sliding(unrealsdk.BL2MOD):
         self.old_z = 0
         self.b_update = False
 
-    def handle_move(self, caller, function, params):
+    @Hook("WillowGame.WillowPlayerController.PlayerWalking.PlayerMove")
+    def handle_move(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct):
         pc = bl2tools.get_player_controller()
+
         slope_delta = pc.Pawn.Location.Z - self.old_z
 
-        if slope_delta / params.DeltaTime > 2.0:
+        if pc.Pawn.Acceleration.X == 0.0 and pc.Pawn.Acceleration.Y == 0.0:  # turn off sliding
+            pc.Pawn.CrouchedPct = self.slide_speed = 0.5
+
+        if slope_delta / params.DeltaTime > 2.0:  # sliding up
             self.slide_duration -= (params.DeltaTime * 1.3)
 
         elif slope_delta / params.DeltaTime < -2.0:  # down
-            self.slide_duration -= (params.DeltaTime / 2)
+            self.slide_duration -= (params.DeltaTime / 5)
 
         else:
             self.slide_duration -= params.DeltaTime
 
-        if pc.bDuck == 1 and self.slide_duration > 0.:
+        if pc.bDuck and self.slide_duration > 0.:
 
-            pc.Rotation.Roll = 700
+            pc.Rotation.Roll = int(self.slide_duration * 700)
             pc.Pawn.CrouchedPct = 2.1
             self.slide_duration -= params.DeltaTime
 
-
-        elif pc.bDuck != 1 or self.slide_duration <= 0.:
+        elif not pc.bDuck or self.slide_duration <= 0.:  # stopped duck or time over
             pc.Rotation.Roll = 0
-            pc.Pawn.CrouchedPct = 0.42
+            pc.Pawn.CrouchedPct = 0.5
 
         self.old_z = pc.Pawn.Location.Z
+        return True
 
-    def handle_duck(self, caller, function, params):
+    @Hook("WillowGame.WillowPlayerInput.DuckPressed")
+    def handle_duck(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct):
         pc = bl2tools.get_player_controller()
         self.old_z = pc.Pawn.Location.Z
         if pc.bInSprintState:
@@ -63,24 +74,6 @@ class Sliding(unrealsdk.BL2MOD):
                 return False
         else:
             return True
-
-    def Enable(self):
-        def DoSlide(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
-            # self.handle_duck(caller, function, params)
-            return True
-
-        def AdvancedMove(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
-            # self.handle_move(caller, function, params)
-            Log("Ayy")
-
-            return True
-
-        unrealsdk.RegisterHook("WillowGame.WillowPlayerInput.DuckPressed", "SlideHook", DoSlide)
-        unrealsdk.RegisterHook("Engine.PlayerController.PCServerMoveInner", "MoveHook", AdvancedMove)
-
-    def Disable(self):
-        unrealsdk.RemoveHook("WillowGame.WillowPlayerInput.DuckPressed", "SlideHook")
-        unrealsdk.RemoveHook("Engine.PlayerController.PCServerMoveInner", "MoveHook")
 
 
 unrealsdk.RegisterMod(Sliding())
