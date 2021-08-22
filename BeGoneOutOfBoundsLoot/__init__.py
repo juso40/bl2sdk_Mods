d965fabbc934795672a13fe66cb1850729dbbe78
Justin Sostmann <justin.sostmann@googlemail.com>
2021-08-22 12:17:26 +0200

    updated BGOOBL

--- BeGoneOutOfBoundsLoot/__init__.py
+++ BeGoneOutOfBoundsLoot/__init__.py
@@ -1,35 +1,43 @@
+from typing import List
+
 import unrealsdk
 
+from ..ModMenu import EnabledSaveType, KeybindManager, SDKMod
 
-class BGOOBL(unrealsdk.BL2MOD):
-    Name = "Be Gone Out Of Bounds Loot"
-    Version = "1.1"
-    Type = [unrealsdk.ModTypes.Utility]
-    Description = "Adds a keybind option to the game that allows you to teleport all loot on the ground to your " \
-                  f"current location. By default the key is binded to ENTER.\n\n{Version}"
-    Author = "Juso"
 
-    Keybinds = [["Teleport Loot To Me", "Enter"]]
+def get_player_controller():
+    return unrealsdk.GetEngine().GamePlayers[0].Actor
 
-    def GetPlayerController(self):
-        return unrealsdk.GetEngine().GamePlayers[0].Actor
 
-    def GameInputPressed(self, input):
-        if input.Name == "Teleport Loot To Me":
-            Pawn = self.GetPlayerController().Pawn
-            Location = (Pawn.Location.X, Pawn.Location.Y, Pawn.Location.Z)
-            for Pickup in self.GetPlayerController().GetWillowGlobals().PickupList:
-                Pickup.Location = Location
+class BGOOBL(SDKMod):
+    Name: str = "Be Gone Out Of Bounds Loot"
+    Version: str = "1.2"
+    Types = unrealsdk.ModTypes.Utility
+    Description: str = "Adds a keybind option to the game that allows you to teleport all loot on the ground to your " \
+                       f"current location. By default the key is binded to ENTER.\n\n{Version}"
+    Author: str = "Juso"
+
+    Keybinds: List[KeybindManager.Keybind] = [KeybindManager.Keybind("Teleport Loot To Me", Key="Enter")]
+    SaveEnabledState: EnabledSaveType = EnabledSaveType.LoadWithSettings
+
+    def GameInputPressed(self, bind: KeybindManager.Keybind, event: KeybindManager.InputEvent) -> None:
+        if event != KeybindManager.InputEvent.Released:
+            return
+        if bind.Name == "Teleport Loot To Me":
+            pawn = get_player_controller().Pawn
+            location = (pawn.Location.X, pawn.Location.Y, pawn.Location.Z)
+            for Pickup in get_player_controller().GetWillowGlobals().PickupList:
+                Pickup.Location = location
                 Pickup.AdjustPickupPhysicsAndCollisionForBeingDropped()
-                lst = list(Location)
+                lst: List[float, float, float] = list(location)
                 lst[2] += 10
-                Location = tuple(lst)
+                location = tuple(lst)
 
     def Enable(self):
-        pass
+        super().Enable()
 
     def Disable(self):
-        pass
+        super().Disable()
 
 
 unrealsdk.RegisterMod(BGOOBL())
