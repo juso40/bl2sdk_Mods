f1541ba575a502e958e9ba9532a07a2a45706857
Justin Sostmann <justin.sostmann@googlemail.com>
2022-06-21 23:24:58 +0200

    Refactor LootNotificator

--- LootNotificator/__init__.py
+++ LootNotificator/__init__.py
@@ -1,14 +1,13 @@
-import bl2sdk
 import unrealsdk
-from Mods import ModMenu
-from Mods.ModMenu import EnabledSaveType, Mods, ModTypes, Options,  OptionManager, RegisterMod, SDKMod, Hook
+from ..ModMenu import EnabledSaveType, ModTypes, OptionManager, SDKMod, Hook, RegisterMod
 
 import os
 import json
 
+
 class Lootbeams(SDKMod):
     Name: str = "Loot Notificator"
-    Version: str = "2.0"
+    Version: str = "2.1"
     Description: str = "Adds Special Particles to all Red Text gear dropped. Also plays a sound. " \
                        "Configure which modpack you are running in the Mod Options."
     Author: str = "Juso and SilverBeam"
@@ -21,63 +20,59 @@ class Lootbeams(SDKMod):
         with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "rarities.json"), "r") as f:
             self.Rarities = json.load(f)
 
-        self.SelectedModpack = "Exodus"
-
         self.Particles = [
-            'FX_ENV_Misc.Particles.Part_Confetti',#Lowest
-            'FX_Aster_Knight.Particles.Paladin.Part_DivineShard_HitImpact', 
-            'FX_Aster_Knight.Particles.Paladin.Part_PillarOfSmite', #Middle Rarity          
-            'FX_Aster_Knight.Particles.Paladin.Part_DivineFavor',  #High Rarity
-            'FX_Aster_Knight.Particles.Paladin.Part_ConcecrateGround', #Highest Rarity     
-            'FX_Aster_ButtStallion.Particles.Part_ButtStallionSavesTheWorld' #Rainbow Rarity
+            "FX_ENV_Misc.Particles.Part_Confetti",  # Lowest
+            "FX_Aster_Knight.Particles.Paladin.Part_DivineShard_HitImpact",
+            "FX_Aster_Knight.Particles.Paladin.Part_PillarOfSmite",  # Middle Rarity
+            "FX_Aster_Knight.Particles.Paladin.Part_DivineFavor",  # High Rarity
+            "FX_Aster_Knight.Particles.Paladin.Part_ConcecrateGround",  # Highest Rarity
+            "FX_Aster_ButtStallion.Particles.Part_ButtStallionSavesTheWorld"  # Rainbow Rarity
         ]
 
         self.EventSounds = [
-            'Ake_UI.UI_Mission.Ak_Play_UI_Mission_Reward',
-            'Ake_UI.UI_HUD.Ak_Play_UI_HUD_Chapter_Stinger'
+            "Ake_UI.UI_Mission.Ak_Play_UI_Mission_Reward",
+            "Ake_UI.UI_HUD.Ak_Play_UI_HUD_Chapter_Stinger"
         ]
 
+        self.ModPackSpinner = OptionManager.Options.Spinner(
+            Caption="Mod Pack",
+            Description="Select which modpack you are running.",
+            StartingValue="Exodus",
+            Choices=list(self.Rarities.keys())
+        )
         self.Options = [
-            OptionManager.Options.Spinner(
-                "Mod Pack",
-                f"Select which modpack you are running.",
-                StartingValue="Exodus",
-                Choices=list(self.Rarities.keys())
-            )
+            self.ModPackSpinner
         ]
 
-    def ModOptionChanged(self, option, new_value):
-        if option.Caption == "Mod Pack":
-            self.SelectedModpack = new_value   
-           
-    # This is how we know that we're in the main menu. We run the hook on the first tick of the main menu, then we unhook.
+    # This is how we know that we're in the main menu.
+    # We run the hook on the first tick of the main menu, then we unhook.
     # This is needed due to the fact that not all packages are available to load until the MainMenu has loaded.
-    @Hook("WillowGame.FrontendGFxMovie.OnTick","ForceLoadParticles")
+    @Hook("WillowGame.FrontendGFxMovie.OnTick", "ForceLoadParticles")
     def ForceLoad(self, caller, function, params):
-        bl2sdk.LoadPackage("SanctuaryAir_Dynamic")
-        bl2sdk.LoadPackage("CastleKeep_FX")
-        bl2sdk.LoadPackage("CastleKeep_Combat")
-        bl2sdk.LoadPackage("CastleKeep_Mission")
+        unrealsdk.LoadPackage("SanctuaryAir_Dynamic")
+        unrealsdk.LoadPackage("CastleKeep_FX")
+        unrealsdk.LoadPackage("CastleKeep_Combat")
+        unrealsdk.LoadPackage("CastleKeep_Mission")
         for Particle in self.Particles:
-            temp = bl2sdk.FindObject("ParticleSystem", Particle)
-            bl2sdk.KeepAlive(temp)
-        unrealsdk.RemoveHook("WillowGame.FrontendGFxMovie.OnTick","ForceLoadParticles")
+            temp = unrealsdk.FindObject("ParticleSystem", Particle)
+            unrealsdk.KeepAlive(temp)
+        unrealsdk.RemoveHook("WillowGame.FrontendGFxMovie.OnTick", "ForceLoadParticles")
 
-    def GetParticle(self, index): 
-        return bl2sdk.FindObject("ParticleSystem", self.Particles[index])
+    def GetParticle(self, index):
+        return unrealsdk.FindObject("ParticleSystem", self.Particles[index])
 
-    def GetSound(self,index):
-        return bl2sdk.FindObject("AkEvent", self.EventSounds[index])
+    def GetSound(self, index):
+        return unrealsdk.FindObject("AkEvent", self.EventSounds[index])
 
     @Hook("WillowGame.WillowPickup.ConvertRigidBodyToFixed")
     def HandleLootBeams(self, caller, function, params):
-        #We search for the EmitterPool to spawn our particles from, also we need a location.
-        EmitterSpawner = bl2sdk.GetEngine().GetCurrentWorldInfo().MyEmitterPool
+        # We search for the EmitterPool to spawn our particles from, also we need a location.
+        EmitterSpawner = unrealsdk.GetEngine().GetCurrentWorldInfo().MyEmitterPool
         Location = (caller.Location.X, caller.Location.Y, caller.Location.Z)
         Rotation = (caller.Rotation.Pitch, caller.Rotation.Yaw, caller.Rotation.Roll)
-        #If the RBState Position is 0 it means its not on the ground
+        # If the RBState Position is 0 it means it's not on the ground
         if caller.RBState.Position.X != 0:
-            for rarity in self.Rarities[self.SelectedModpack]:
+            for rarity in self.Rarities[self.ModPackSpinner.CurrentValue]:
                 if caller.InventoryRarityLevel in range(rarity["min_level"], rarity["max_level"] + 1):
                     EmitterSpawner.SpawnEmitter(self.GetParticle(rarity["particle"]), Location, Rotation)
                     caller.PlayAkEvent(self.GetSound(rarity["sound"]))
@@ -85,8 +80,9 @@ class Lootbeams(SDKMod):
 
     def Enable(self):
         super().Enable()
+
     def Disable(self):
         super().Disable()
 
 
-unrealsdk.RegisterMod(Lootbeams())
+RegisterMod(Lootbeams())
