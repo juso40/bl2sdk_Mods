d75539da1f5564595cf06060bdf5cded1bfabf42
Justin Sostmann <justin.sostmann@googlemail.com>
2022-08-05 21:40:13 +0200

    Update Photomode to v1.1

--- Photomode/__init__.py
+++ Photomode/__init__.py
@@ -1,58 +1,105 @@
-from unrealsdk import *
+from typing import Optional, Any
 
-from . import bl2tools
+import unrealsdk
+
+from ..ModMenu import EnabledSaveType, SDKMod, RegisterMod, ModTypes, KeybindManager
 
 
-class Photo(BL2MOD):
+class Photo(SDKMod):
     Name = "Photomode"
-    Version = "1.0"
-    Types = [ModTypes.Utility]
-    Description = f"Simple Photo Mode for BL2.\n\n{Version}"
+    Version = "1.1"
+    Types = ModTypes.Utility
+    Description = """Simple Photo Mode for BL2.
+'Mousewheel Up/Down' to change the active modifier.
+Hold 'Shift' while scrolling to change the camera movement speed.
+Hold 'R' while scrolling to change the camera rotation.
+Hold 'F' while scrolling to change the camera FOV.
+"""
     Author = "Juso"
+    SaveEnabledState = EnabledSaveType.LoadWithSettings
 
-    Keybinds = []
+    Keybinds = [
+        KeybindManager.Keybind("Photomode", "P", IsRebindable=True),
+        KeybindManager.Keybind("Modifier +", "MouseScrollUp", IsRebindable=False),
+        KeybindManager.Keybind("Modifier -", "MouseScrollDown", IsRebindable=False),
+        KeybindManager.Keybind("Camera Speed", "Shift", IsRebindable=False),
+        KeybindManager.Keybind("Roll (Camera Rotation)", "R", IsRebindable=False),
+        KeybindManager.Keybind("FOV", "F", IsRebindable=False),
+    ]
+
     def __init__(self):
-        self.b_photo = False
-        self.pawn = None
-        self.Keybinds = [["Photomode", "P"],
-                         ["Photomode Roll+", "MouseScrollUp"],
-                         ["Photomode Roll-", "MouseScrollDown"]]
+        self.in_photomode: bool = False
+        self.pawn_backup: Optional[unrealsdk.UObject] = None
+        self.active_camera_modifier: callable = lambda x: None
+        self.last_modifier_name: str = ""
 
-    def GameInputPressed(self, input):
-        if input.Name == "Photomode":
-            pc = bl2tools.get_player_controller()
-            world_info = GetEngine().GetCurrentWorldInfo()
-            if world_info.GetStreamingPersistentMapName() != "menumap":
-                if self.b_photo and world_info.bPlayersOnly:
-                    pc.Possess(self.pawn, True)
-                    pc.DisplayHUD()
-                    world_info.bPlayersOnly = False
-                    pc.Rotation.Roll = 0
-                    self.b_photo = False
-                else:
-                    self.pawn = pc.Pawn
-                    pc.Unpossess()
-                    pc.HideHUD()
-                    pc.ServerSpectate()
-                    world_info.bPlayersOnly = True
-                    self.b_photo = True
-        elif input.Name == "Photomode Roll+":
-            pc = bl2tools.get_player_controller()
-            world_info = GetEngine().GetCurrentWorldInfo()
-            if self.b_photo and world_info.bPlayersOnly:
-                bl2tools.get_player_controller().Rotation.Roll += 128
-        elif input.Name == "Photomode Roll-":
-            pc = bl2tools.get_player_controller()
-            world_info = GetEngine().GetCurrentWorldInfo()
-            if self.b_photo and world_info.bPlayersOnly:
-                bl2tools.get_player_controller().Rotation.Roll -= 128
+    @property
+    def pc(self) -> unrealsdk.UObject:
+        return unrealsdk.GetEngine().GamePlayers[0].Actor
 
-    def Enable(self):
-        self.Keybinds = [("Photomode", "P"),
-                         ("Photomode Roll+", "MouseScrollUp"),
-                         ("Photomode Roll-", "MouseScrollDown")]
+    def toggle_photo_mode(self, enable: bool) -> None:
+        world_info: unrealsdk.UObject = unrealsdk.GetEngine().GetCurrentWorldInfo()
+        if enable:
+            self.pawn_backup = self.pc.Pawn
+            self.pc.Unpossess()
+            self.pc.HideHUD()
+            self.pc.ServerSpectate()
+            world_info.bPlayersOnly = True
+            self.in_photomode = True
+        else:
+            self.pc.Possess(self.pawn_backup, True)
+            self.pc.DisplayHUD()
+            world_info.bPlayersOnly = False
+            self.pc.Rotation.Roll = 0
+            self.pc.SetFOV(self.pc.DefaultFOV)
+            self.in_photomode = False
 
-    def Disable(self):
-        self.Keybinds = []
+    def GameInputPressed(self, bind: KeybindManager.Keybind, event: KeybindManager.InputEvent) -> None:
+        if event == KeybindManager.InputEvent.Released:
+
+            if bind.Name == "Photomode":
+                world_info: unrealsdk.UObject = unrealsdk.GetEngine().GetCurrentWorldInfo()
+                if world_info.GetStreamingPersistentMapName() == "menumap":
+                    return
+                self.toggle_photo_mode(
+                    not (self.in_photomode and world_info.bPlayersOnly)
+                )
+            elif bind.Name == "Modifier +":
+                self.active_camera_modifier(1)
+            elif bind.Name == "Modifier -":
+                self.active_camera_modifier(-1)
+
+            if bind.Name == self.last_modifier_name:
+                self.last_modifier_name = ""
+                self.active_camera_modifier = lambda x: None
+
+        elif event == KeybindManager.InputEvent.Pressed and self.in_photomode:
+
+            if bind.Name == "Camera Speed":
+                self.active_camera_modifier = self.camera_speed_modifier
+                self.last_modifier_name = bind.Name
+            elif bind.Name == "Roll (Camera Rotation)":
+                self.active_camera_modifier = self.camera_roll_modifier
+                self.last_modifier_name = bind.Name
+            elif bind.Name == "FOV":
+                self.active_camera_modifier = self.camera_fov_modifier
+                self.last_modifier_name = bind.Name
+
+    def camera_roll_modifier(self, x: int) -> None:
+        self.pc.Rotation.Roll += x * 128
+
+    def camera_speed_modifier(self, x: int) -> None:
+        speed: float = self.pc.CameraManager.MovementSpeed
+        # clamp between 50 and 10000
+        speed = max(min(speed + x * 50, 10000), 50)
+
+        self.pc.CameraManager.MovementSpeed = speed
+
+    def camera_fov_modifier(self, x: int) -> None:
+        fov: int = self.pc.FOVAngle
+        # clamp between 4 and 180
+        fov = max(min(fov + x, 180), 4)
+        self.pc.SetFOV(fov)
+
 
 RegisterMod(Photo())
