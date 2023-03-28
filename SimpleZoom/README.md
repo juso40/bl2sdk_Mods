c3db1b0cdf8458ee86d76f41790f0619d8739bc8
Justin Sostmann <justin.sostmann@googlemail.com>
2023-03-28 16:29:32 +0200

    Reformat SimpleZoom, add readme

---
+++ SimpleZoom/README.md
@@ -0,0 +1,3 @@
+# SimpleZoom
+Small mod that adds a keybind to zoom the camera in and out.   
+The zoom modifier can be configured in the mods options menu.  
--- SimpleZoom/__init__.py
+++ SimpleZoom/__init__.py
@@ -1,7 +1,16 @@
 from typing import List
-import unrealsdk
 
-from ..ModMenu import EnabledSaveType, SDKMod, RegisterMod, ModTypes, KeybindManager, OptionManager, Game
+import unrealsdk  # type: ignore
+
+from Mods.ModMenu import (
+    EnabledSaveType,
+    Game,
+    KeybindManager,
+    ModTypes,
+    OptionManager,
+    RegisterMod,
+    SDKMod,
+)
 
 
 class SimpleZoom(SDKMod):
@@ -17,22 +26,24 @@ class SimpleZoom(SDKMod):
         KeybindManager.Keybind("Zoom", "C"),
     ]
 
-    Options: List[OptionManager.Options.Base] = [
+    Options: List[OptionManager.Options.Slider] = [
         OptionManager.Options.Slider("Zoom FOV", "The FOV to zoom to.", 15, 1, 60, 1),
     ]
 
-    def GameInputPressed(self, bind: KeybindManager.Keybind, event: KeybindManager.InputEvent) -> None:
+    def GameInputPressed(  # noqa: N802
+        self, bind: KeybindManager.Keybind, event: KeybindManager.InputEvent
+    ) -> None:
         if bind.Name == "Zoom" and event == KeybindManager.InputEvent.Pressed:
             self.Zoom()
         elif bind.Name == "Zoom" and event == KeybindManager.InputEvent.Released:
             self.Unzoom()
 
-    def Zoom(self) -> None:
+    def Zoom(self) -> None:  # noqa: N802
         pc = unrealsdk.GetEngine().GamePlayers[0].Actor
         pc.LowerFirstPersonHands()
         pc.SetFOV(self.Options[0].CurrentValue)
 
-    def Unzoom(self) -> None:
+    def Unzoom(self) -> None:  # noqa: N802
         pc = unrealsdk.GetEngine().GamePlayers[0].Actor
         pc.RaiseFirstPersonHands()
         pc.SetFOV(pc.DefaultFOV)
