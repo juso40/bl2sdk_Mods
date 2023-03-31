7df32766653ae2da7cb1c78071aad972082bdf54
Justin Sostmann <justin.sostmann@googlemail.com>
2023-03-31 14:15:45 +0200

    Update Sliding to 2.2

---
+++ Sliding/README.md
@@ -0,0 +1,6 @@
+# Sliding
+*Adds sliding to BL2/TPS/AoDK.*  
+Pressing crouch while sprinting will make you slide.
+If you jump while sliding, you will jump keep the momentum, which generally makes you go further.  
+Sliding down slopes will accelerate you, while sliding up slopes will slow you down.
+
--- Sliding/__init__.py
+++ Sliding/__init__.py
@@ -1,113 +1,244 @@
-from typing import ClassVar
+import unrealsdk  # type: ignore
 
-import unrealsdk
+from Mods.ModMenu import EnabledSaveType, Game, Hook, ModTypes, SDKMod
+from Mods.tweens import (
+    Tween,
+    circ_out,
+    cubic_in_out,
+    cubic_out,
+    elastic_out,
+    quad_out,
+)
+from Mods.uemath import Vector
 
-from ..ModMenu import SDKMod, Hook, EnabledSaveType, ModTypes
 
 class Sliding(SDKMod):
     Name: str = "Sliding"
     Description: str = "Sliding in BL2."
     Author: str = "Juso"
-    Version: str = "2.1"
+    Version: str = "2.2"
     Types: ModTypes = ModTypes.Gameplay
     SaveEnabledState: EnabledSaveType = EnabledSaveType.LoadWithSettings
-
-    PACKAGE_NAME: ClassVar[str] = "Sliding"
-    ATTR_PREFIX: ClassVar[str] = "Attr_"
-    MODIFIER_PREFIX: ClassVar[str] = "Modifier_"
-    MOVE_SPEED_SUFFIX: ClassVar[str] = "MoveSpeed"
-
-    MOVE_SPEED_ATTRIBUTE_NAME: ClassVar[str] = "D_Attributes.GameplayAttributes.FootSpeed"
+    SupportedGames: Game = Game.BL2 | Game.AoDK | Game.TPS
 
     def __init__(self):
-        self.slide_duration = 2.0
+        self.do_slide_jump: bool = False
+        self.horizontal_velocity: Vector = Vector()
+        self.slide_speed: float = 2.2
+        self.old_z: float = 0
+        self.is_sliding: bool = False
+        self.tweener: Tween = Tween()
+
+    def tween_slide(self, pc: unrealsdk.UObject) -> None:
+        if self.tweener.is_running():
+            self.tweener.kill()
+        arms = pc.Pawn.Arms
+        if not arms.Attachments:
+            return
+        self.tweener = Tween()
+        t = self.tweener
+        t.tween_property(
+            arms.SkeletalMesh.RotOrigin,
+            "Pitch",
+            final_value=500,
+            duration=0.2,
+        ).from_current().transition(cubic_in_out)
+        t.tween_property(
+            arms.SkeletalMesh.RotOrigin,
+            "Yaw",
+            final_value=-200,
+            duration=0.4,
+        ).from_current().transition(quad_out)
+        t.tween_property(
+            arms.SkeletalMesh.RotOrigin,
+            "Roll",
+            final_value=-6300,
+            duration=0.5,
+        ).from_current().transition(cubic_out)
+        t.tween_property(
+            arms.SkeletalMesh.Origin,
+            "X",
+            final_value=30,
+            duration=1.2,
+        ).from_current().transition(elastic_out)
+        t.tween_property(
+            arms.SkeletalMesh.Origin,
+            "Y",
+            final_value=-14.5,
+            duration=0.5,
+        ).from_current().transition(circ_out)
+        t.tween_property(
+            arms.SkeletalMesh.Origin,
+            "Z",
+            final_value=-175,
+            duration=0.5,
+        ).from_current().transition(circ_out)
+        t.set_parallel(True)
+        t.start()
+
+    def tween_reset(self, pc: unrealsdk.UObject) -> None:
+        if self.tweener.is_running():
+            self.tweener.kill()
+        arms = pc.Pawn.Arms
+        if not arms.Attachments:
+            return
+        self.tweener = Tween()
+        t = self.tweener
+        self.tweener = Tween()
+        t = self.tweener
+        t.tween_property(
+            arms.SkeletalMesh.RotOrigin,
+            "Pitch",
+            final_value=0,
+            duration=0.5,
+        ).from_current().transition(cubic_in_out)
+        t.tween_property(
+            arms.SkeletalMesh.RotOrigin,
+            "Yaw",
+            final_value=0,
+            duration=0.4,
+        ).from_current().transition(quad_out)
+        t.tween_property(
+            arms.SkeletalMesh.RotOrigin,
+            "Roll",
+            final_value=0,
+            duration=0.3,
+        ).from_current().transition(cubic_in_out)
+        t.tween_property(
+            arms.SkeletalMesh.Origin,
+            "X",
+            final_value=40,
+            duration=0.4,
+        ).from_current().transition(circ_out)
+        t.tween_property(
+            arms.SkeletalMesh.Origin,
+            "Y",
+            final_value=0,
+            duration=0.6,
+        ).from_current().transition(circ_out)
+        t.tween_property(
+            arms.SkeletalMesh.Origin,
+            "Z",
+            final_value=-167,
+            duration=0.5,
+        ).from_current().transition(circ_out)
+        t.set_parallel(True)
+        t.start()
+
+    def exit_slide(self, pc: unrealsdk.UObject) -> None:
+        if not self.is_sliding:
+            return
+        self.is_sliding = False
         self.slide_speed = 2.2
-        self.old_z = 0
-        self.speed_modifier = None
-        self.speed_attribute = None
+        unrealsdk.CallPostEdit(False)
+        pc.Pawn.CrouchedPct = 0.5
+        unrealsdk.CallPostEdit(True)
+        self.tween_reset(pc)
 
-    def Enable(self) -> None:
-        super().Enable()
-        self.create_objects()
+    def enter_slide(self, pc: unrealsdk.UObject) -> None:
+        if self.is_sliding:
+            return
+        self.is_sliding = True
+        self.old_z = pc.Pawn.Location.Z
+        self.slide_speed = 2.2
+        unrealsdk.CallPostEdit(False)
+        pc.Pawn.CrouchedPct = self.slide_speed
+        unrealsdk.CallPostEdit(True)
+        self.tween_slide(pc)
 
-    def create_objects(self) -> None:
-        # Thanks to apples Mario mod for figuring out how to do this
-        package = unrealsdk.FindObject("Package", self.PACKAGE_NAME)
-        if package is None:
-            package = unrealsdk.ConstructObject(
-                Class="Package",
-                Outer=None,
-                Name=self.PACKAGE_NAME,
-            )
-            unrealsdk.KeepAlive(package)
+    def slide(
+        self,
+        pc: unrealsdk.UObject,
+        delta_time: float,
+    ) -> None:
+        # z_diff is the height difference between the current frame and the last frame in cm (Unreal units)
+        z_diff: float = pc.Pawn.Location.Z - self.old_z
 
-        # Find MoveSpeed AttributeDefinition
-        self.speed_attribute: unrealsdk.UObject = unrealsdk.FindObject(
-            "AttributeDefinition",
-            self.MOVE_SPEED_ATTRIBUTE_NAME
-        )
-
-        # Find or create SlideSpeed AttributeModifer
-        self.speed_modifier: unrealsdk.UObject = unrealsdk.FindObject(
-            "AttributeModifier",
-            f"{self.PACKAGE_NAME}.Modifier_SlideSpeed"
-        )
-        if self.speed_modifier is None:
-            self.speed_modifier = unrealsdk.ConstructObject(
-                Class="AttributeModifier",
-                Outer=package,
-                Name="Modifier_SlideSpeed"
-            )
-            unrealsdk.KeepAlive(self.speed_modifier)
-            self.speed_modifier.Type = 0  # MT_Scale
-            self.speed_modifier.Value = self.slide_speed
-
-    def update_speed_modifier(self) -> None:
-        self.speed_modifier.Value = self.slide_speed
-
-    @Hook("WillowGame.WillowPlayerController.PlayerWalking.PlayerMove")
-    def handle_move(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct):
-        pc = caller
-        # Remove the modifier. Each Tick is a bit too much, but eh. Don't really care
-        self.speed_attribute.RemoveAttributeModifier(pc.Pawn, self.speed_modifier)
-
-        slope_delta = pc.Pawn.Location.Z - self.old_z
-        if pc.Pawn.Acceleration.X == 0.0 and pc.Pawn.Acceleration.Y == 0.0:  # turn off sliding
-            self.slide_duration = 0.0
-
-        if (slope_delta * params.DeltaTime) > 0.0005:  # sliding up
-            self.slide_duration -= (params.DeltaTime * 1.3)
-            self.slide_speed -= (0.5 * params.DeltaTime)
-
-        elif (slope_delta * params.DeltaTime) < -0.0005:  # down
-            self.slide_duration -= (params.DeltaTime / 5)
-
-        else:
-            self.slide_duration -= params.DeltaTime
-            self.slide_speed -= (0.3 * params.DeltaTime)
-
-        # We are only sliding while we are ducked on the ground and moving
-        if pc.bDuck and self.slide_duration > 0. and pc.Pawn.IsOnGroundOrShortFall():
-            pc.Rotation.Roll = int(self.slide_duration * 700)  # Calculate Rotation Roll
-
-            # While sliding re add the Attribute
-            self.update_speed_modifier()
-            self.speed_attribute.AddAttributeModifier(pc.Pawn, self.speed_modifier)
-            self.slide_duration -= params.DeltaTime  # Remove time from max slide duration
-
-        else:  # stopped duck or time over
-            pc.Rotation.Roll = max(0, int(pc.Rotation.Roll - 3 * 700 * params.DeltaTime))  # Calculate Rotation Roll
+        # We generally want to slow down over time, but if we are going up a slope, we want to slow down even more
+        # Slididng down a slope should slightly increase the speed
+        if z_diff < 0:  # We are going down a slope
+            self.slide_speed -= z_diff * 0.0005
+        else:  # We are going up a slope or on flat ground
+            self.slide_speed -= delta_time * 0.7 + z_diff * 0.004
 
         self.old_z = pc.Pawn.Location.Z
+        pc.Pawn.CrouchedPct = self.slide_speed
+
+    def can_slide(self, pc: unrealsdk.UObject, pawn: unrealsdk.UObject) -> bool:
+        accel = pawn.Acceleration
+        return (
+            self.is_sliding
+            and pc.bDuck
+            and pawn.IsOnGroundOrShortFall()
+            and not (accel.X == 0.0 and accel.Y == 0.0)
+        )
+
+    @Hook("WillowGame.WillowPlayerInput.Jump")
+    def jump(
+        self,
+        caller: unrealsdk.UObject,
+        function: unrealsdk.UFunction,
+        params: unrealsdk.FStruct,
+    ) -> bool:
+        if self.is_sliding:
+            pc = caller.Outer
+            vel: Vector = Vector(pc.Pawn.Velocity)
+            vel.z = 0
+            self.horizontal_velocity = vel
+            self.do_slide_jump = True
+        return True
+
+    @Hook("WillowGame.WillowPlayerController.PlayerWalking.PlayerMove")
+    def handle_move(
+        self,
+        caller: unrealsdk.UObject,
+        function: unrealsdk.UFunction,
+        params: unrealsdk.FStruct,
+    ):
+        pc: unrealsdk.UObject = caller
+        pawn: unrealsdk.UObject = pc.Pawn
+
+        # We most likely wont pass our slide exit conditions after pressing jump,
+        # as that will cause the player to stand up
+        # So we need to do the slide jump as one of the first things.
+        if self.do_slide_jump:
+            if pawn.IsOnGroundOrShortFall():
+                pawn.DoJump(True)
+            else:
+                self.do_slide_jump = False
+
+                unrealsdk.CallPostEdit(False)
+                pawn.Velocity.X = self.horizontal_velocity.x
+                pawn.Velocity.Y = self.horizontal_velocity.y
+                unrealsdk.CallPostEdit(True)
+                return True
+
+        # Check our exit conditions
+        if not self.can_slide(pc, pawn):
+            self.exit_slide(pc)
+            return True
+
+        # We are sliding
+        # If we dont call CallPostEdit(False) we will for whatever reason remove all status effects from the player
+        unrealsdk.CallPostEdit(False)
+        self.slide(pc, params.DeltaTime)
+        unrealsdk.CallPostEdit(True)
+
+        # After actually sliding, check if we are still fast enough to slide
+        if self.slide_speed < 0.5:
+            self.exit_slide(pc)
         return True
 
     @Hook("WillowGame.WillowPlayerInput.DuckPressed")
-    def handle_duck(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct):
+    def handle_duck(
+        self,
+        caller: unrealsdk.UObject,
+        function: unrealsdk.UFunction,
+        params: unrealsdk.FStruct,
+    ):
         pc = caller.Outer
-        self.old_z = pc.Pawn.Location.Z
         if pc.bInSprintState:
-            self.slide_duration = 2.0  # Reset slide duration
-            self.slide_speed = 2.2  # Reset slide speed
+            self.enter_slide(pc)
         return True
 
 
